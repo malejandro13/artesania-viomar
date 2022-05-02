6c25437262841011a0ea9c3741059211a6e3f6c2
Marcelo Alejandro <msv_326@outlook.com>
2022-05-02 18:10:24 -0400

    chore: add typography enum in interface file

--- src/components/UI/atoms/Typography/Typography.tsx
+++ src/components/UI/atoms/Typography/Typography.tsx
@@ -1,8 +1,11 @@
-import { TypographyElements } from "@constants/typography";
-import { TypographyProps } from "./Typography.interfaces";
+import { TypographyElements, TypographyProps } from "./Typography.interfaces";
 import { Text } from "./Typography.styled";
 
-export const Typography = ({ variant, color, children }: TypographyProps) => {
+export const Typography = ({
+  variant = TypographyElements.h1,
+  color,
+  children,
+}: TypographyProps) => {
   const elementType = TypographyElements[variant as keyof typeof TypographyElements];
 
   return (
