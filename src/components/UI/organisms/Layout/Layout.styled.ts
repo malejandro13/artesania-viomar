c723a02edef70c09fc4cc60af3c5afaba1658b3c
Marcelo Alejandro <msv_326@outlook.com>
2022-05-07 16:57:40 -0400

    feat: add layout organism

---
+++ src/components/UI/organisms/Layout/Layout.styled.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { AlignItems, SectionProps } from './Layout.interfaces';
+
+export const Section =styled.section<SectionProps>`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: ${({ alignItems }) => alignItems || AlignItems.center};
+  overflow-x: hidden;
+  padding: 20px;
+  padding-bottom: 120px;
+  padding-top: 100px;
+  background: #bb9a83;
+  background: linear-gradient(to right, #7f695c, #bb9a83);
+  overflow: hidden;
+
+  @media (min-width: ${({theme: { breakpoints: { large: { value, unit }}}}) => `${value}${unit}`}) {
+    padding: 70px 50px;
+
+    &::before {
+      content: '';
+      position: fixed;
+      top: -10rem;
+      right: -20rem;
+      width: 700px;
+      height: 700px;
+      border-radius: 50%;
+      border: 14rem solid rgba(0, 0, 0, .05);
+      
+    }
+  }
+`;
