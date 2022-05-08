8bc2f7d296fd91186d749a8062880481c80b7220
Marcelo Alejandro <msv_326@outlook.com>
2022-05-07 21:44:03 -0400

    chore: change facebook link

--- src/utils/constants/socialMedia.ts
+++ src/utils/constants/socialMedia.ts
@@ -3,7 +3,7 @@ import { Instagram } from '@components/icons/Instagram';
 import { Whatsapp } from '@components/icons/Whatsapp';
 
 export const SOCIAL_MEDIA_LIST = [
-  {name: 'facebook', link: 'https://www.facebook.com/Artesan%C3%ADa-Viomar-1198766723519948/', Icon: Facebook},
+  {name: 'facebook', link: 'https://www.facebook.com/Artesan%C3%ADa-Viomar-110025248354544', Icon: Facebook},
   {name: 'instagram', link: 'https://www.instagram.com/artesania_viomar/', Icon: Instagram},
   {name: 'whatsapp', link: 'https://wa.me/56935091952', Icon: Whatsapp},
 ];
