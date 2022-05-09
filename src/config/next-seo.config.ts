91d228ac6db700f26b79094f39cd3d27a66e595e
Marcelo Alejandro <msv_326@outlook.com>
2022-05-09 10:55:53 -0400

    feat: add next seo

---
+++ src/config/next-seo.config.ts
@@ -0,0 +1,24 @@
+const nextSeoConfig = {
+  title: "Artesanía Viomar",
+  description:
+    "Nos dedicamos al diseño y confección de artesanía en cuero y lana, donde podrás solicitar productos exclusivamente diseñados para ti y adaptados a tus gustos.",
+  canonical: "https://artesaniaviomar.cl/",
+  openGraph: {
+    type: "website",
+    url: "https://artesaniaviomar.cl/",
+    title: "Artesanía Viomar",
+    description:
+      "Nos dedicamos al diseño y confección de artesanía en cuero y lana, donde podrás solicitar productos exclusivamente diseñados para ti y adaptados a tus gustos.",
+    images: [
+      {
+        url: "/assets/images/artesania-viomar.jpg",
+        width: 1000,
+        height: 1019,
+        alt: "Artesanía Viomar"
+      }
+    ],
+    site_name: "Artesanía Viomar"
+  },
+};
+
+export default nextSeoConfig;
