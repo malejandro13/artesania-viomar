d7d49cb9f06992e79386b9ea02c4097327acc8bb
Marcelo Alejandro <msv_326@outlook.com>
2022-05-09 13:03:55 -0400

    feat: add pwa

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -38,8 +38,8 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <meta name="theme-color" content="#553f31" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <meta name="theme-color" content="#7f695c" />
         </Head>
         <body>
           <Main />
